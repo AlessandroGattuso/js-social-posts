d485aaa0e051af309be43f29b0e0170f9d7e97ab
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2022-12-15 22:19:21 +0100

    added comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Create array of posts
 let posts = [
   {
     authorName: 'Pippo Cartesio',
@@ -20,20 +21,22 @@ let posts = [
   }
 ];
 
+//add date and id properties to to every obj of the array
 posts.forEach((post, i) => {
   post.date = generateDate();
   post.date = formatDate(post.date)
   post.id = i+1;
 });
 
-const cards = document.querySelector('.card-container');
 
+const cards = document.querySelector('.card-container');
 
 posts.forEach((post) =>{
   let img = "";
   let profileImg;
   let footerCard;
 
+  //If the image exist as property of the actual object diplay it in the post inside th page
   if(post.image != undefined){
     img = `<img src="${post.image}" class="card-img" alt="Image not found"></img>`;
     footerCard = `<div class="card-body p-0 d-flex gap-5 w-100 justify-content-center mt-5">`
@@ -41,14 +44,16 @@ posts.forEach((post) =>{
   else
     footerCard = `<div class="card-body p-0 d-flex gap-5 w-100 justify-content-center mt-3">`
     
-
+  //If the profile picture as property of the actual object display it in the post inside the page
   if(post.authorPic != undefined)
     profileImg = `<img src="${post.authorPic}" class="profile-pic" alt="Image not found"></img>`;
+  //Else create a div and put inside of it the initials of the author's name
   else{
     let nameInitials = post.authorName.split(" ").map((e)=>e[0]).join("");
     profileImg = `<div class="profile-pic d-flex align-items-center justify-content-center fs-2 text-white">${nameInitials}</div>`
   }
   
+  //append the element
   cards.innerHTML += `
               <div class="card p-3 myCard">
                 <div class="card-body p-0 mb-3">
@@ -72,34 +77,35 @@ posts.forEach((post) =>{
 
 const like = document.querySelectorAll('.card button');
 const likeText = document.querySelectorAll('.like-text');
-let postLiked = [];
+
+//Array of which posts the user liked
+let postsLiked = [];
+
 
 like.forEach((button,i) => {
 
   button.addEventListener('click',function(){
+    //if the button is not already been clicked add class 'active' to the button, increment the number of likes, update the like-text and at the end push the id of the post inside postsLiked
     if(!this.classList.contains('active')){
       this.classList.add('active')
       likeText[i].innerText = ++posts[i].n_likes;
-      postLiked.push(i);
+      postsLiked.push(i+1);
     } 
+    //else remove class 'active' to the button, decrement the number of likes, update the like-text and at the end remove the id of the post inside postLiked
     else{
       this.classList.remove('active')
       likeText[i].innerText = --posts[i].n_likes;
-      postLiked = postLiked.filter((likeItem)=> likeItem!=i);
+      postsLiked = postsLiked.filter((likeItem)=> likeItem!=i+1);
     }
   })
 
 })
 
-function formatDate(date) {
-    const formatted = date.split("-");
-    return formatted[1] + "/" + formatted[0] + "/" + formatted[2];
-}
-
+//function we use to generate a random date from 2012 until current year
 function generateDate(){
   let day = Math.floor(Math.random() * (32 - 1) + 1);
   let month = Math.floor(Math.random() * (13 - 1) + 1);
-  const year = Math.floor(Math.random() * (2023 - 2012) + 2012);
+  const year = Math.floor(Math.random() * (parseInt((new Date().getFullYear())) + 1 - 2012) + 2012);
 
   if(day < 10)
     day = "0" + day;
@@ -107,4 +113,10 @@ function generateDate(){
     month = "0" + month;
 
   return month + "-" + day + "-" + year;
+}
+
+//function we use to format the date to: 'gg/mm/yyyy'
+function formatDate(date) {
+    const formatted = date.split("-");
+    return formatted[1] + "/" + formatted[0] + "/" + formatted[2];
 }
